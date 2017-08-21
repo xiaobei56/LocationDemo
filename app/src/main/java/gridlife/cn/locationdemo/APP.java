b27600bbbba793aac160c53a61e91f98c1a1709f
xiaobei56 <xiaobei_63@163.com>
2017-08-21 17:43:13 +0800

    Initial commit

---
+++ app/src/main/java/gridlife/cn/locationdemo/APP.java
@@ -0,0 +1,17 @@
+package gridlife.cn.locationdemo;
+
+import android.app.Application;
+
+/**
+ * ProjectName LocationDemo
+ * PackageName gridlife.cn.locationdemo
+ * Created by damaren_bzb on 2017/8/21.
+ */
+
+public class APP extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
